c63daacbafe2529148a4da93d91a68a72e68441d
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-03-13 18:50:00 +1100

    fix: temp remove main.ml's main

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,9 @@
 open Frontend
-
+(* 
 let rec print_list = function 
   [] -> ()
   | e::l -> print_string (Tokens.show_token e); print_string " "; print_list l
 
 let main () = print_list (Lexer.tokenize "fun hello() { if () {} else {} }")
 
-let () = main ()
+let () = main () *)

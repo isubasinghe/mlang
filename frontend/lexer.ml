eca25c42ed8fd12fd6661afb2d11ec3b59801406
isubasinghe <isubasinghe@student.unimelb.edu.au>
2021-03-13 18:50:28 +1100

    adapted lexer to work with new parser

--- frontend/lexer.ml
+++ frontend/lexer.ml
@@ -1,5 +1,5 @@
 open List
-
+open Parser
 let letter = [%sedlex.regexp? 'a'..'z'|'A'..'Z']
 let integer = [%sedlex.regexp? '0'..'9']
 let float = [%sedlex.regexp? (Plus integer), '.', (Plus integer)]
@@ -8,24 +8,23 @@ let rec token buf tokens =
   
   match%sedlex buf with
     | white_space -> (); token buf tokens
-    | float -> (); token buf ((Tokens.T_Float (Sedlexing.Latin1.lexeme buf)) :: tokens)
-    | Plus integer -> (); token buf ((Tokens.T_Int (Sedlexing.Latin1.lexeme buf)) :: tokens)
-    | "if" -> (); token buf ((Tokens.T_If)::tokens)
-    | "fun" -> (); token buf ((Tokens.T_Fun)::tokens)
-    | "for" -> (); token buf ((Tokens.T_For)::tokens)
-    | "else" -> (); token buf ((Tokens.T_Else)::tokens)
-    | "int" -> (); token buf ((Tokens.T_TInt)::tokens)
-    | "{" -> (); token buf ((Tokens.T_LBrace)::tokens)
-    | "}" -> (); token buf ((Tokens.T_RBrace)::tokens)
-    | "[" -> (); token buf ((Tokens.T_LBrack)::tokens)
-    | "]" -> (); token buf ((Tokens.T_RBrack)::tokens)
-    | "(" -> (); token buf ((Tokens.T_LParan)::tokens)
-    | ")" -> (); token buf ((Tokens.T_RParan)::tokens)
-    | ";" -> (); token buf ((Tokens.T_SemiColon)::tokens)
-    | letter, Star (letter | integer) -> (); token buf ((Tokens.T_Id (Sedlexing.Latin1.lexeme buf) )::tokens)
+    | float -> (); token buf (FLOAT (float_of_string (Sedlexing.Latin1.lexeme buf))::tokens)
+    | Plus integer -> (); token buf (INT (int_of_string (Sedlexing.Latin1.lexeme buf))::tokens)
+    | "if" -> (); token buf tokens
+    | "fun" -> (); token buf tokens
+    | "for" -> (); token buf tokens
+    | "else" -> (); token buf tokens
+    | "int" -> (); token buf tokens
+    | "{" -> (); token buf tokens
+    | "}" -> (); token buf tokens
+    | "[" -> (); token buf tokens
+    | "]" -> (); token buf tokens
+    | "(" -> (); token buf tokens
+    | ")" -> (); token buf tokens
+    | ";" -> (); token buf tokens
+    | letter, Star (letter | integer) -> (); token buf tokens
     | eof -> rev tokens
     | _ -> failwith "Internal failure: Reached impossible place"
-
 
 let tokenize s =
   let lexbuf = Sedlexing.Utf8.from_string s in
